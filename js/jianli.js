59cedba4370cef0acb2a22a38739255f463c0999
siting321 <68320491+siting321@users.noreply.github.com>
2020-07-18 12:35:32 +0800

    add img css js fonts

---
+++ js/jianli.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	$('#btn').hide(); //隐藏go to top按钮
+
+	$(window).scroll(function() {
+		// console.log($(this).scrollTop());
+
+		//当window的scrolltop距离大于1时，go to 
+		if ($(this).scrollTop() > 100) {
+			$('#btn').fadeIn();
+		} else {
+			$('#btn').fadeOut();
+		}
+	});
+
+	$('#btn').click(function() {
+		$('html ,body').animate({
+			scrollTop: 0
+		}, 300);
+		return false;
+	});
+});
+$('body').scrollspy({ target: '#navbar-example' })
